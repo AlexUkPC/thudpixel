0cb9c15d8874d8ebd4f5a1dc79f0559899513d5e
AlexUkPC <alexrogna@yahoo.com>
2021-04-16 17:34:49 +0100

    Comments added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :shots
+  resources :shots do
+    resource :comments
+  end
   devise_for :users, controllers:{registrations:'registrations'}	
   root 'shots#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_16_161407) do
+ActiveRecord::Schema.define(version: 2021_04_16_162759) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "response"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "shot_id"
+    t.integer "user_id"
+  end
 
   create_table "shots", force: :cascade do |t|
     t.string "title"
